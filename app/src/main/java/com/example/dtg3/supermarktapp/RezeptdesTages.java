cf5e51dc74276520fb6121909b0ec1eafdbec9ec
Joe <johannes.idelberger@googlemail.com>
2020-07-04 10:32:48 +0200

    added ui elements

---
+++ app/src/main/java/com/example/dtg3/supermarktapp/RezeptdesTages.java
@@ -0,0 +1,13 @@
+package com.example.dtg3.supermarktapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RezeptdesTages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rezeptdes_tages);
+    }
+}
